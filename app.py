ed8099d80bc66a7393434c18982dcab5cde8bb21
gin <takaoueda1998@gmail.com>
2019-10-27 04:22:00 +0900

    fix

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def handle_message(event):
 def handle_location(event):
     latitude = event.message.latitude
     longitude = event.message.longitude
-    results = get_shops_data(lng=longitude,lat=latitude,types="convenience_store",radius=200)
+    results = get_shops_data(lng=longitude,lat=latitude,types="convenience_store",radius=1000)
     print("###########")
     print(results)
     shops = Shops(results["results"])
