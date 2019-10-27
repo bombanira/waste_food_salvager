d866f0b9d38878fbc631bd5b7c9d39de010e7a26
gin <takaoueda1998@gmail.com>
2019-10-27 10:50:25 +0900

    require

--- app.py
+++ app.py
@@ -84,24 +84,162 @@ def handle_message(event):
         ###
         return 
     if event.message.text == "hello":
-        url = "https://api.line.me/v2/bot/message/reply"
-        userID = event.source.user_id
-        data = {
-            'to' : userID,
-            'messages' : [
-                {
-                'type' : 'text',
-                'text' : 'Hello, world! from api'
+        payload = {
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+                "type": "bubble",
+                "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                    "type": "uri",
+                    "label": "Line",
+                    "uri": "https://linecorp.com/"
                 }
-            ]
-        }
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + YOUR_CHANNEL_ACCESS_TOKEN
-        }
-        req = urllib.request.Request(url, json.dumps(data).encode(), headers)
-        with urllib.request.urlopen(req) as res:
-            body = res.read()
+                },
+                "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "Brown Cafe",
+                    "size": "xl",
+                    "weight": "bold"
+                    },
+                    {
+                    "type": "box",
+                    "layout": "baseline",
+                    "margin": "md",
+                    "contents": [
+                        {
+                        "type": "icon",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "icon",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "icon",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "icon",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "icon",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                        "size": "sm"
+                        },
+                        {
+                        "type": "text",
+                        "text": "4.0",
+                        "flex": 0,
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#999999"
+                        }
+                    ]
+                    },
+                    {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "margin": "lg",
+                    "contents": [
+                        {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                            "type": "text",
+                            "text": "Place",
+                            "flex": 1,
+                            "size": "sm",
+                            "color": "#AAAAAA"
+                            },
+                            {
+                            "type": "text",
+                            "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                            "flex": 5,
+                            "size": "sm",
+                            "color": "#666666",
+                            "wrap": True
+                            }
+                        ]
+                        },
+                        {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                            "type": "text",
+                            "text": "Time",
+                            "flex": 1,
+                            "size": "sm",
+                            "color": "#AAAAAA"
+                            },
+                            {
+                            "type": "text",
+                            "text": "10:00 - 23:00",
+                            "flex": 5,
+                            "size": "sm",
+                            "color": "#666666",
+                            "wrap": True
+                            }
+                        ]
+                        }
+                    ]
+                    }
+                ]
+                },
+                "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "flex": 0,
+                "spacing": "sm",
+                "contents": [
+                    {
+                    "type": "button",
+                    "action": {
+                        "type": "uri",
+                        "label": "CALL",
+                        "uri": "https://linecorp.com"
+                    },
+                    "height": "sm",
+                    "style": "link"
+                    },
+                    {
+                    "type": "button",
+                    "action": {
+                        "type": "uri",
+                        "label": "WEBSITE",
+                        "uri": "https://linecorp.com"
+                    },
+                    "height": "sm",
+                    "style": "link"
+                    },
+                    {
+                    "type": "spacer",
+                    "size": "sm"
+                    }
+                ]
+                }
+            }
+            }
+        container_obj = FlexSendMessage.new_from_json_dict(payload)
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
